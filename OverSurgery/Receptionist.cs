1ca6a589ed3f5f71a5c3966a57a9dfb1e0568904
Senaz-Dhom <zanes.tune1757@gmail.com>
2017-11-12 18:48:31 +0800

    Add class Receptionist

---
+++ OverSurgery/Receptionist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverSurgery
+{
+    class Receptionist
+    {
+        int id;
+        string name;
+        int status;
+        DateTime dateJoined;
+        string loginName;
+        string password;
+    }
+}
